8152cad73d3ef9907b9e1e864a1b695cda1bb691
Nandan Bhunia <nandan.bhunia25@outlook.com>
2026-04-07 22:56:31 +0530

    new feauture added

--- app/app.py
+++ app/app.py
@@ -3,7 +3,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Azure CI/CD Pipeline 🚀"
+    return "Azure CI/CD Pipeline build successful! This is a test application to verify the deployment process. " \
+    "Congratulations on setting up your pipeline correctly!  Feel free to customize this message or add more routes to your application as needed.   This is a great starting point for your Azure CI/CD pipeline, and you can expand it with additional features or functionality as you continue to develop your application." \
+    " Keep up the great work and happy coding! "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
